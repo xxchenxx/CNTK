fb88bee91a5098c81299a436cb7c8893d12892a8
Ch4nYH <hfercxx@hotmail.com>
2022-01-07 19:07:36 +0800

    update

--- resnet18.py
+++ resnet18.py
@@ -3,16 +3,21 @@ import neural_tangents as nt
 from utils import load_cifar
 import numpy as np
 import sys
+import jax
 from jax import random
 key1, key2 = random.split(random.PRNGKey(int(sys.argv[1])), 2)
 init_fn, apply_fn = ResNet18(num_classes=10, img_dim=(32, 32))
 (X_train, y_train), (X_test, y_test) = load_cifar()
-X = np.concatenate((X_train, X_test), axis = 0)
-N = X.shape[0]
-N_train = X_train.shape[0]
-N_test = X_test.shape[0]
+X_train = jax.ndarray.array(X_train)
+X_test = jax.ndarray.array(X_test)
+y_train = jax.ndarray.array(y_train)
+y_test = jax.ndarray.array(y_test)
+
+
+print(X_train.shape)
 
 kernel_fn = nt.empirical_kernel_fn(apply_fn, vmap_axes=0, implementation=2)
+kernel_fn_batched = nt.batch(kernel_fn, device_count=-1, batch_size=5)
 _, params = init_fn(key1, X_train.shape)
 k_test_train = kernel_fn(X_test, X_train, params)
 k_train_train = kernel_fn(X_train, None, params)
@@ -20,7 +25,7 @@ k_train_train = kernel_fn(X_train, None, params)
 from jax.example_libraries import stax
 from neural_tangents import predict
 cross_entropy = lambda fx, y_hat: -np.mean(stax.logsoftmax(fx) * y_hat)
-predict_fn = predict.gradient_descent(cross_entropy, k_test_train,
+predict_fn = predict.gradient_descent(cross_entropy, k_train_train,
                                       y_train, 0.01, 0.9)
 
 fx_train_0 = apply_fn(params, X_train)
