c2a9187a94fc31bf6332bbea6ea240966cd7d071
Ch4nYH <hfercxx@hotmail.com>
2022-01-07 18:50:16 +0800

    update

--- resnet18.py
+++ resnet18.py
@@ -14,6 +14,19 @@ N_test = X_test.shape[0]
 
 kernel_fn = nt.empirical_kernel_fn(apply_fn, vmap_axes=0, implementation=2)
 _, params = init_fn(key1, X_train.shape)
-predict_fn = nt.predict.gradient_descent_mse_ensemble(kernel_fn, X_train,
-                                                      y_train)
-y_test_ntk = predict_fn(x_test=X_test, get='ntk')
+k_test_train = kernel_fn(X_test, X_train, params)
+k_train_train = kernel_fn(X_train, None, params)
+
+from jax.example_libraries import stax
+from neural_tangents import predict
+cross_entropy = lambda fx, y_hat: -np.mean(stax.logsoftmax(fx) * y_hat)
+predict_fn = predict.gradient_descent(cross_entropy, k_test_train,
+                                      y_train, 0.01, 0.9)
+
+fx_train_0 = apply_fn(params, X_train)
+fx_test_0 = apply_fn(params, X_test)
+
+fx_train_t, fx_test_t = predict_fn(1e-7, fx_train_0, fx_test_0,
+                                   k_test_train)
+import pickle
+pickle.dump([fx_train_t, fx_test_t], open(f"{sys.argv[1]}.pkl", "wb"))
